c358aec29a7aa4117147166d7656048032e37b02
Tomek <tomek7sa@gmail.com>
2017-12-29 18:23:35 +0100

    Tutorial nr 9

--- spring-tutorial-5/src/main/java/com/caveofprogramming/spring/test/Person.java
+++ spring-tutorial-5/src/main/java/com/caveofprogramming/spring/test/Person.java
@@ -5,6 +5,8 @@ public class Person {
 	private int id;
 	private String name;
 	
+	private int taxId;
+	
 	public Person(int id, String name) {
 		this.id = id;
 		this.name = name;
@@ -16,15 +18,16 @@ public class Person {
 	
 	@Override
 	public String toString() {
-		return "Person [id=" + id + ", name=" + name + "]";
+		return "Person [id=" + id + ", name=" + name + ", taxId=" + taxId + "]";
 	}
-
-
-
-
-
 
 	public void speak() {
 		System.out.println("Hello! I'm a person.");
 	}
+
+	public void setTaxId(int taxId) {
+		this.taxId = taxId;
+	}
+	
+	
 }
