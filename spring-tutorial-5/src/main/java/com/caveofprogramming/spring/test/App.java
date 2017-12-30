70babc73d2d2c90fa8bb656a528efa6b837393dd
Tomek <tomek7sa@gmail.com>
2017-12-30 10:25:44 +0100

    Named paramars in sql query

--- spring-tutorial-5/src/main/java/com/caveofprogramming/spring/test/App.java
+++ spring-tutorial-5/src/main/java/com/caveofprogramming/spring/test/App.java
@@ -1,9 +1,12 @@
 package com.caveofprogramming.spring.test;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
 
 public class App {
 
@@ -11,9 +14,27 @@ public class App {
 		
 		ApplicationContext context  = new ClassPathXmlApplicationContext("com/caveofprogramming/spring/test/beans/beans.xml");
 		
-		Robot robot = (Robot)context.getBean("robot");
 		
-		robot.speak();
+		
+		OffersDAO offersDao = (OffersDAO)context.getBean("offersDao");
+		
+		try {
+			List<Offer> offers = offersDao.getOffers();
+			
+			for (Offer offer: offers) {
+				System.out.println(offer);
+			}
+		} 
+		catch( CannotGetJdbcConnectionException ex) {
+			System.out.println("Cannot get database connecion.\n");
+		} 
+		catch (DataAccessException ex) {
+			System.out.println(ex.getMessage());
+			System.out.println(ex.getClass());
+		}
+		
+		
+		
 		
 		((ClassPathXmlApplicationContext)context).close();
 
