870686603aa47b06186c9ee8dc5877902c6af74e
Tomek <tomek7sa@gmail.com>
2017-12-30 14:53:14 +0100

    Losts of changes with changes

--- WebContent/WEB-INF/jsps/home.jsp
+++ WebContent/WEB-INF/jsps/home.jsp
@@ -1,5 +1,8 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/sql" prefix="sql" %>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -7,6 +10,17 @@
 <title>Insert title here</title>
 </head>
 <body>
-Hi there!
+<c:out value="${name}"></c:out>
+
+<sql:query var="rs" dataSource="jdbc/spring">
+select id, name, email, text from offers
+</sql:query>
+
+<c:forEach var="row" items="${rs.rows}">
+    ID: ${row.id}<br/>
+    Name: ${row.name}<br/>
+</c:forEach>
+
+
 </body>
 </html>
---
+++ src/com/caveofprogramming/spring/web/controllers/OffersController.java
@@ -0,0 +1,39 @@
+package com.caveofprogramming.spring.web.controllers;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class OffersController {
+
+	/*
+	@RequestMapping("/")
+	public ModelAndView showHome(HttpSession session) {
+		
+		ModelAndView mv = new ModelAndView("home");
+		
+		Map<String, Object> model = mv.getModel();
+		
+		model.put("name", "<b>River</b>");
+		
+		return mv;
+	}
+	*/
+	
+	@RequestMapping("/")
+	public String showHome(Model model) {
+		
+		model.addAttribute("name", "Tiffany");
+		
+		return "home";
+	}
+	
+}
+
+
