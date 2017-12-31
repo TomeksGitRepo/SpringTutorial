a308d46463352dec3785991ada2f5be7c51fcc62
Tomek <tomek7sa@gmail.com>
2017-12-31 10:19:06 +0100

    First stage of offers site finished.

--- WebContent/WEB-INF/jsps/offercreated.jsp
+++ WebContent/WEB-INF/jsps/offercreated.jsp
@@ -1,7 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -9,9 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-<p> <a href="${pageContext.request.contextPath}/offers">Show current offers.</a></p>
-<p> <a href="${pageContext.request.contextPath}/createoffer">Add new offer</a></p>
-
-
+Offer created: <a href="${ pageContext.request.contextPath }/offers"> click here to vie all offers</a>
 </body>
 </html>
--- src/com/caveofprogramming/spring/web/controllers/OffersController.java
+++ src/com/caveofprogramming/spring/web/controllers/OffersController.java
@@ -1,39 +1,86 @@
 package com.caveofprogramming.spring.web.controllers;
 
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.caveofprogramming.spring.web.dao.Offer;
+import com.caveofprogramming.spring.web.service.OffersService;
 
 @Controller
 public class OffersController {
 
-	/*
-	@RequestMapping("/")
-	public ModelAndView showHome(HttpSession session) {
-		
-		ModelAndView mv = new ModelAndView("home");
-		
-		Map<String, Object> model = mv.getModel();
-		
-		model.put("name", "<b>River</b>");
-		
-		return mv;
-	}
-	*/
+	private OffersService offersService;
 	
-	@RequestMapping("/")
-	public String showHome(Model model) {
+	@RequestMapping("/offers")
+	public String showOffers(Model model) {
 		
-		model.addAttribute("name", "Tiffany");
+		//offersService.throwTestException();
 		
+		List<Offer> offers = offersService.getCurrent();
+		
+		model.addAttribute("offers", offers);
+		
+		return "offers";
+	}
+	@RequestMapping(value="/test", method=RequestMethod.GET)
+	public String showTest(Model model, @RequestParam("id")String id) {
+		
+		
+		System.out.println("ID is " + id);
 		return "home";
 	}
 	
+	@RequestMapping("/createoffer")
+	public String createOffer(Model model) {
+		
+		model.addAttribute("offer", new Offer());
+		
+		return "createoffer";
+	}
+	
+	@RequestMapping(value="/docreate", method=RequestMethod.POST)
+	public String doCreate(Model model, @Valid Offer offer, BindingResult result) {
+		
+		if(result.hasErrors()) {
+			
+			
+			return "createoffer";
+			
+		}
+		
+		offersService.create(offer);
+
+		
+		return "offercreated";
+	}
+	
+
+	@Autowired
+	public void setOffersService(OffersService offersService) {
+		this.offersService = offersService;
+	}
+	
+	/*
+	@ExceptionHandler(DataAccessException.class)
+	public String handleDatabaseException(DataAccessException ex) {
+		return "error";
+	}
+	*/
 }
 
 
